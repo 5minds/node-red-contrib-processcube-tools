1f733ea694331b7d8cad4c9942f5a8cef38a441a
Diana Stefan <diana.stefan@5minds.de>
2025-10-13 13:54:06 +0200

    Add Config for email receiver.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Importiere die Registrierungsfunktion für deine Nodes.
+import registerImapConfigNode from './imap-config/imap-config';
 import registerEmailReceiverNode from './email-receiver/email-receiver';
 import registerEmailSenderNode from './email-sender/email-sender';
 import registerHtmlToTextNode from './html-to-text/html-to-text';
@@ -6,6 +7,7 @@ import registerHtmlToTextNode from './html-to-text/html-to-text';
 // Exportiere eine Funktion, die alle Nodes registriert.
 export = function (RED: any) {
     // Rufe die Registrierungsfunktionen für jede Node auf.
+    registerImapConfigNode(RED);
     registerEmailReceiverNode(RED);
     registerEmailSenderNode(RED);
     registerHtmlToTextNode(RED);
