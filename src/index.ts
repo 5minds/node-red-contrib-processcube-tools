29069a423156841e6bc76e2d94f791d94b3b8589
Diana Stefan <diana.stefan@5minds.de>
2025-10-28 08:13:01 +0100

    Move the config nodes into the Nodes where they are used

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Importiere die Registrierungsfunktion für deine Nodes.
-import registerImapConfigNode from './imap-config/imap-config';
-import registerSmtpConfigNode from './smtp-config/smtp-config';
+import registerImapConfigNode from './email-receiver/imap-config/imap-config';
+import registerSmtpConfigNode from './email-sender/smtp-config/smtp-config';
 import registerEmailReceiverNode from './email-receiver/email-receiver';
 import registerEmailSenderNode from './email-sender/email-sender';
 import registerHtmlToTextNode from './html-to-text/html-to-text';
